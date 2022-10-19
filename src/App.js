5af260340275d4fd8b0ee6f3036b5cfd43b8aeec
Pavithran <pavithrank02@gmail.com>
2022-10-19 07:53:01 +0530

    utilizingm lazy loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import QuoteDetail from './pages/QuoteDetail'; 
-import NotFound from './pages/NotFound';
+
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+
 function App() {
 
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
+     <Suspense fallback={
+      <div className='centered'>
+      <LoadingSpinner />
+      </div>
+     }>
+     <Switch>
+        <Route path='/' exact> 
         <Redirect to='.quotes' />
         </Route>
         <Route path='/quotes' exact>
@@ -25,6 +34,8 @@ function App() {
           <NotFound />
         </Route>
       </Switch>
+     </Suspense>
+      
     </Layout>
     
   );
